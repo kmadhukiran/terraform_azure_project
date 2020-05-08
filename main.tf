92c9812e9b081c936280371e7c4245b2d491b8b8
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-05-08 10:00:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,40 +30,29 @@ resource "azurerm_network_security_group" "myterraformsecurity" {
     name                = "AtmecsSecurity"
     location            = "westus"
     resource_group_name = "${azurerm_resource_group.Atmecs.name}"
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "http"
-        priority                   = "330"
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
- destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "http"
-        priority                   = "330"
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "8080"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+	name 			                    = "AllowSSH"
+	priority 		                  = 100
+	direction 	                  = "Inbound"
+	access 		                    = "Allow"
+	protocol 		                  = "Tcp"
+	source_port_range             = "*"
+    destination_port_range     	= "22"
+    source_address_prefix      	= "*"
+    destination_address_prefix 	= "*"
+  }
 
+  security_rule {
+	name 			                    = "AllowHTTP"
+	priority		                  = 200
+	direction		                  = "Inbound"
+	access 			                  = "Allow"
+	protocol 		                  = "Tcp"
+	source_port_range             = "*"
+    destination_port_range     	= "8080"
+    source_address_prefix      	= "Internet"
+    destination_address_prefix 	= "*"
+  }
 }
 resource "azurerm_network_interface" "myterraformNIC" {
   name                = "AtmecsNICgroup"
