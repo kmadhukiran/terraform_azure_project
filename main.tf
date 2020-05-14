d96bb8db7e4e2c8a15cadf765808e34abd59e382
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-05-14 10:08:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,34 +23,6 @@ resource "azurerm_subnet" "myterraformsubnet" {
     virtual_network_name = "AtmecsVnet"
     address_prefix       = "10.0.1.0/24"
 }
-resource "azurerm_network_security_group" "myterraformsecurity" {
-    name                = "AtmecsSecurity"
-    location            = "westus"
-    resource_group_name = "${azurerm_resource_group.Atmecs.name}"
-  security_rule {
-        name                    = "AllowSSH"
-        priority                = 100
-        direction               = "Inbound"
-        access                  = "Allow"
-        protocol                = "Tcp"
-        source_port_range       = "*"
-    destination_port_range      = "22"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-  }
-
-  security_rule {
-        name                    = "AllowHTTP"
-        priority                = 200
-        direction               = "Inbound"
-        access                  = "Allow" 
-        protocol                = "Tcp"
-        source_port_range       = "*"
-    destination_port_range      = "8080"
-    source_address_prefix       = "Internet"
-    destination_address_prefix  = "*"
-  }
-}
 resource "azurerm_network_interface" "myterraformNIC" {
   name                = "AtmecsNICgroup"
   location            = "westus"
