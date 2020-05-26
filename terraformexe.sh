409949d017cfdb6ba58511cd391566577dc14dbf
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-05-26 15:14:24 +0530

    Update terraformexe.sh

--- terraformexe.sh
+++ terraformexe.sh
@@ -7,11 +7,13 @@ pipeline {
             git credentialsId: 'Atmecs', url: 'https://github.com/kmadhukiran/terraform_azure_project.git'
          }
       }
+         node{
          stage('Deploy') {
         withCredentials([azureServicePrincipal('Azure_credentials')]) {
             sh 'az login --service-principal -u $Client ID -p $Client Secret -t $Tenant ID'
             sh 'az account set -s $Subscription ID'
             sh 'az resource list'
+            }
         }
     }
 }
