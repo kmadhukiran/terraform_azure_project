afe0684bcb5878d7efb219fdf34e56f05e7d23d6
kmadhukiran <58160350+kmadhukiran@users.noreply.github.com>
2020-05-26 15:17:07 +0530

    Update terraformexe.sh

--- terraformexe.sh
+++ terraformexe.sh
@@ -1,7 +1,7 @@
      node{
          stage('Deploy') {
         withCredentials([azureServicePrincipal('Azure_credentials')]) {
-            sh 'az login --service-principal -u $Client ID -p $Client Secret -t $Tenant ID'
+            sh 'az login --service-principal -u $Client ID -p $Client Secret -t $Tenant ID ----only-show-errors'
             sh 'az account set -s $Subscription ID'
             sh 'az resource list'
             }
